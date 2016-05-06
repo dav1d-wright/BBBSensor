05208913a4de009ee94509623dc8f59a5fd72150
David Wright <david.wright@bluewin.ch>
2016-05-06 15:41:24 +0200

    + Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 6 May 2016
+ *      Author: wright
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello hard float world!" << endl;
+	return 0;
+}
